e751c2a1ca4ca468c52e64310374825413704560
FernandoMMattos <etcloide@gmail.com>
2025-12-29 18:42:49 +0000

    nextjs and home page
    updated nextjs
    made the home page content responsive

--- portfolio/src/app/page.tsx
+++ portfolio/src/app/page.tsx
@@ -2,13 +2,13 @@ import Image from "next/image";
 
 const Home = () => {
   return (
-    <main className="flex-grow content-center">
-      <section className="flex p-8 justify-around items-center">
-        <div className="w-1/2 flex flex-col gap-3">
+    <main className="flex-grow content-center select-none">
+      <section className="flex p-8 justify-around items-center flex-col md:flex-row gap-4">
+        <div className="md:w-1/2 flex flex-col gap-3 w-fit">
           <h1 className="text-4xl">
             <strong className="text-(--accent)">Fernando</strong> portfolio.
           </h1>
-          <p className="text-xl whitespace-pre-line leading-relaxed;">
+          <p className="text-xl whitespace-pre-line leading-relaxed">
             {`Hi, I’m Fernando Mattos
             A Front-End Developer with a degree in Computer Science and +2,000 hours of hands-on practice in React, Next.js, TypeScript, and Firebase. Passionate about building clean and user-friendly web applications.`}
           </p>
